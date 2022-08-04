ce9ef29d42d96f17546e4dc77c924e0148bbf64b
Eduardo Farias <eduardofarias@Eduardo-Farias.local>
2022-08-04 02:51:36 -0300

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import Presentation1 from './assets/presentation_farm_1.jpg';
+import Presentation2 from './assets/presentation_farm_2.jpg';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Testing Image Imports</Text>
+      <Image source={Presentation1} style={{width: 300, height: 150}}/>
+      <Image source={Presentation2} style={{width: 300, height: 150, marginTop: 20}}/>
       <StatusBar style="auto" />
     </View>
   );
